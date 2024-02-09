a8470d9df809e480f624cc7e1485520ca70f3b88
Omar Solares <118589833+OmarSolares21@users.noreply.github.com>
2024-02-09 06:48:52 -0700

    Update cis129_lab02_shortStory.py
    my short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('This is my first cis project of 2024') 
+print("Once upon a time, in a dense forest, a lone adventurer wandered beneath the setting sun.")
+print("Suddenly, a rustling in the bushes caught their attention, sending a shiver down their spine.")
+print("With cautious steps, they approached, only to discover a playful fox, its eyes sparkling in the dim light.")
+print("Relieved, the adventurer shared a chuckle with the curious creature before continuing on their journey, filled with newfound courage.")
+
+
+
+
+
